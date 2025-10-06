5201d3efb963d6d2caa85ff27c2e1ca51c668d83
skangyam <skangyam@opentext.com>
2025-10-06 21:02:26 +0530

    Hardcoded supabase creds

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+// export const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// );
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  'https://omfjfnzkmrglzaytdzls.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tZmpmbnprbXJnbHpheXRkemxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3Mzc4OTMsImV4cCI6MjA3NTMxMzg5M30.tIzUZ3VxD8fynL3JZJ7zGqOIamYQZ5Wn-eCCfv7ud2M'
 );
